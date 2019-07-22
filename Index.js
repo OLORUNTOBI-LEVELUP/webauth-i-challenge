1f0c5bc10696690126a422d4f59f57c61bb0b0ce
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-22 13:22:15 +0100

    POST/register endpoint working

--- Index.js
+++ Index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
+const registerRoute = require("./routes/users")
 const port = process.env.PORT || 4000
 
 
 server.use(express.json());
+server.use('/', registerRoute);
 
 
 
