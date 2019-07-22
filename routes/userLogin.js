7d259be63d1333b7a905ec98f1dbb04196b3b033
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-22 15:57:43 +0100

    Implemented middleware and refactored my code

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -1,34 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const user = require('../database/Users/models')
+const user = require("../database/Users/models");
 const bycrpt = require("bcrypt");
-const joi = require('joi');
 
+router.post("/login", validateUser, validateLogin, (req, res) => {
+  const user = req.user;
+  try {
+    res.status(200).json({ message: `Welcome ${user.username}!` });
+  } catch (error) {
+    res
+      .status(500)
+      .json("Oops! We missed that. Hang on, let's fix it together");
+  }
+});
 
-router.post('/login', (req, res) => {
-  
-    let { username, password} = req.body
-    user.findBy({ username })
+function validateUser(req, res, next) {
+  if (Object.keys(req.body).length !== 0 && req.body.constructor === Object) {
+    if (req.body.username && req.body.password) {
+      next();
+    } else {
+      res
+        .status(400)
+        .json({
+          message:
+            "Nahhhhh! You missed the required username and/or password fields"
+        });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ message: "You must be kidding! Where is the user data?" });
+  }
+}
+
+function validateLogin(req, res, next) {
+  let { username, password } = req.body;
+  user.findBy({ username })
     .first()
     .then(user => {
       if (user && bycrpt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}! You are succesfully logged in` });
+        req.user = user;
+        next();
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Oops! Invalid Credentials" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(() => {
+      res.status(401).json({ message: "Oops! Invalid Credentials" });
     });
-})
+}
 
-function validateUser(req, res, next){
-    const schema = {
-        username: joi.string().min(5).max(50).required(),
-        password: joi.string().min(5).max(255).required()
-    }
-    return joi.validate(req, schema);
-    
-    }
-    
-    module.exports = router;
+module.exports = router;
